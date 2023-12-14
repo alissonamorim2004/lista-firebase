1923c9e5bb4e15e6cd3a1e86a898050ff4545c75
alissonamorim2004 <126924602+alissonamorim2004@users.noreply.github.com>
2023-12-14 08:39:38 -0400

    Add files via upload

---
+++ listata/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDAjWtiO_a8TjCkK8rP_fVul0BXrlvYNvk",
+    authDomain: "lista-2e5ad.firebaseapp.com",
+    projectId: "lista-2e5ad",
+    storageBucket: "lista-2e5ad.appspot.com",
+    messagingSenderId: "660893300425",
+    appId: "1:660893300425:web:98dc6c8e364d0439bdff18"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
